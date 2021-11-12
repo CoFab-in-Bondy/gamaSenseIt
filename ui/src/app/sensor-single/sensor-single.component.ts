382449ed11ae1fd26eb546cb20bc6545711c2345
Dashstrom <incerti.felix@gmail.com>
2021-11-12 19:52:02 +0100

    add parameters view and download

--- ui/src/app/sensor-single/sensor-single.component.ts
+++ ui/src/app/sensor-single/sensor-single.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ParameterService } from '../services/parameter.service';
 import { SensorService } from '../services/sensor.service';
 
 @Component({
@@ -10,18 +12,34 @@ import { SensorService } from '../services/sensor.service';
 export class SensorSingleComponent implements OnInit {
 
   @Input() name: string;
+  id: number;
+  parameters: any[] = [];
 
   constructor(private sensorsService: SensorService,
+              private parametersService: ParameterService,
               private route: ActivatedRoute,
-              private router: Router) { }
+              private router: Router,
+              private http: HttpClient) { }
 
   ngOnInit(): void {
-    const id = this.route.snapshot.params['id'];
-    let sensor = this.sensorsService.getSensorsById(+id);
+    this.id = +this.route.snapshot.params['id'];
+    let sensor = this.sensorsService.getSensorsById(this.id);
     if (!sensor) {
       this.router.navigate(["/error404"]);
       return;
     }
     this.name = sensor.name;
+    this.parametersService.getSensorParameters(this.id).subscribe(
+        (res) => this.parameters = res,
+        (err: any) => console.error(err)
+    );
+  }
+
+  onDownloadCSV() {
+    this.parametersService.dowloadSensorParameters(this.id, "csv");
+  }
+
+  onDownloadJSON() {
+    this.parametersService.dowloadSensorParameters(this.id, "json");
   }
 }
--- ui/src/app/services/parameter.service.ts
+++ ui/src/app/services/parameter.service.ts
@@ -1,8 +1,28 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
 
 @Injectable()
 export class ParameterService {
 
-    constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
+
+  getSensorParameters(sensorId: number): Observable<any[]>{
+    return this.httpClient.get<any[]>(
+      "/public/parameters", 
+      {params: {sensorId: sensorId}}
+    )
+  }
+
+  dowloadSensorParameters(sensorId: number, type: "csv" | "json"): void {
+    window.open("/public/parameters?sensorId=" + sensorId + "&type=" + type, '_blank');
+  }
+
+  private download(data: Blob): void {
+    // const blob = new Blob([data], { type: "text/plain" });
+    console.log(data);
+    const url = window.URL.createObjectURL(data);
+    window.open(url);
+    window.URL.revokeObjectURL(url);
+  }
 }
